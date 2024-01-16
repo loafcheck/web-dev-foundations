919956b9de21f53f75410128f5bae5c5a4536721
Rachel <rachelskim.contact@gmail.com>
2024-01-15 18:24:48 -0800

    lecture 26

--- lecture26.js
+++ lecture26.js
@@ -1,21 +1,15 @@
 
 let btn = document.getElementsByClassName('tab-button');
-let tab = document.getElementsByClassName('tab-content');
+var tab = document.getElementsByClassName('tab-content');
 let list = document.querySelector('.list');
-//윈도우 클릭하면
-//오렌지랑 쇼우 다 지워줘 
-//누른곳이 버튼[ㄱ]
-//오렌지랑 쇼우 클래스 추가해줘
-window.addEventListener('click', function(event) {
-    for(let i = 0 ; i < btn.length ; i++){
-        btn[i].classList.remove('orange');
-        tab[i].classList.remove('show');
-    }
-    for (let j = 0 ; j < tab.length ; j++) {
-        if(event.target === btn[j]){
-            btn[j].classList.add('orange');
-            tab[j].classList.add('show');
+
+window.addEventListener('click',function(event){
+        if(event.target === document.querySelectorAll('.tab-button')[event.target.dataset.id]){
+            tabWork(event.target.dataset.id);
         }
-    }
-    
-});
+})
+
+function tabWork(num) {
+   console.log(num);
+
+}
