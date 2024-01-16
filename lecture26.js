3966b8e3224d9fc1839c76821aef1ca9863cf874
Rachel <rachelskim.contact@gmail.com>
2024-01-16 11:46:53 -0800

    lecture 26

--- lecture26.js
+++ lecture26.js
@@ -4,12 +4,17 @@ var tab = document.getElementsByClassName('tab-content');
 let list = document.querySelector('.list');
 
 window.addEventListener('click',function(event){
-        if(event.target === document.querySelectorAll('.tab-button')[event.target.dataset.id]){
+        if(event.target === btn[event.target.dataset.id]){
             tabWork(event.target.dataset.id);
         }
 })
 
 function tabWork(num) {
+    for(let i = 0 ; i < btn.length ; i++ ){
+        btn[i].classList.remove('orange');
+        tab[i].classList.remove('show');
+    }
+    btn[num].classList.add('orange');
+    tab[num].classList.add('show');
    console.log(num);
-
 }
