8aff36704001adad9d71fdba5b2299ab171e0532
Rachel <rachelskim.contact@gmail.com>
2024-01-20 15:42:59 -0800

    lecture 37

--- lecture36.js
+++ lecture36.js
@@ -6,51 +6,80 @@ var products = [
 
 var templateArray = [];
 var container = document.createElement('div');
+let moreBtn = document.createElement('button');
+let sortBtn = document.createElement('button');
+moreBtn.textContent = 'MORE';
+sortBtn.innerHTML = 'Price low to high';
 document.body.appendChild(container);
+
 
 products.forEach((data, index) => {
     var template = `
         <div class="col-sm-4">
             <h5>${data.title}</h5>
-            <p>${data.price}</p>
+            <p>${products[index].price}</p>
         </div>
     `;
     templateArray.push(template);
+
 });
 
-let moreBtn = document.createElement('button');
-moreBtn.textContent = 'MORE';
+/*
+templateArray 에서 <p>
+*/
+var sortedProducts = [];
+sortBtn.addEventListener('click', function() {
+    products.sort((a,b)=>{
+        return a.price - b.price});
+    console.log(products);
+    sortedProducts = [...products];
+    console.log(sortedProducts);
+    templateArray = [];
+    sortedProducts.forEach((data,index)=>{
+       template2 = `
+        <div class="col-sm-4">
+            <h5>${data.title}</h5>
+            <p>${products[index].price}</p>
+        </div>
+    `;
+    templateArray.push(template2);
+    })
+    
+    console.log(templateArray); 
+    container.innerHTML = templateArray.join('')
 
-moreBtn.addEventListener('click', function () {
-    fetch('https://codingapple1.github.io/js/more1.json')
-        .then(res => res.json())
-        .then(data => {
-            var products2 = data;
+})
 
-            // Assuming template2, template3, etc., are generated similarly
-            var template2 = '';
-            products2.forEach((data, index) => {
-                template2 += `
-                    <div class="col-sm-4">
-                        <h5>${data.title}</h5>
-                        <p>${data.price}</p>
-                    </div>
-                `;
-            });
 
-            // Add new templates to the array
-            templateArray.push(template2);
-
-            // Concatenate all templates in the array
-            container.innerHTML = templateArray.join('');
-        })
-        .catch(error => {
-            console.log(error);
-        });
-});
-
-// Initial append of templateArray to the container
 container.innerHTML = templateArray.join('');
-
-// Append the "MORE" button to the container
 container.appendChild(moreBtn);
+container.appendChild(sortBtn);
+
+
+// moreBtn.addEventListener('click', function () {
+//     fetch('https://codingapple1.github.io/js/more1.json')
+//         .then(res => res.json())
+//         .then(data => {
+//             var products2 = data;
+
+//             // Assuming template2, template3, etc., are generated similarly
+//             products2.forEach((data, index) => {
+//                 var template = `
+//                     <div class="col-sm-4">
+//                         <h5>${data.title}</h5>
+//                         <p>${products2[index].price}</p>
+//                     </div>
+//                 `;
+//                 templateArray.push(template);
+//             });
+
+//             // Render the updated templates and "MORE" button
+//             renderTemplates();
+
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// });
+
+
