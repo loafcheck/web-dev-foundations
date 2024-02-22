81234bd43d1c8b66d375e4dfc809ce4f93c512f9
loafcheck <rachelskim.contact@gmail.com>
2024-02-22 15:24:12 -0800

    lecture 44

--- lecture44.js
+++ lecture44.js
@@ -2,34 +2,37 @@ let currentSlide = 0;
 const slides = document.querySelectorAll('.carousel-slide');
 const prevButton = document.querySelector('.prev-btn');
 const nextButton = document.querySelector('.next-btn');
+let isDragging = false;
+let startX = 0;
 
 function showSlide(n) {
-    //showSlide(1)
-  slides[currentSlide].style.display = 'none';
-  //slides[0].styel.display='none';
-  //slides[0].style.display='none';
-  //slides[1].style.display ='none';
-  //slides[2].style.display = 'none';
-  currentSlide = (n + slides.length) % slides.length;
-  //currentSlide = (0 + 3) % 3 -> 0
-  //currentSlide = (1 + 3) % 3 -> 1
-  //currentSlide = (2 + 3) % 3 -> 2
-  //currentSlide = (3 + 3) % 3 -> 0
-  slides[currentSlide].style.display = 'block';
-  //slides[0].style.display = 'block'
-  //slides[1].style.display = 'block'
-  //slides[2].style.display = 'block'
+    slides[currentSlide].style.display = 'none';
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].style.display = 'block';
 }
 
 prevButton.addEventListener('click', () => {
-  showSlide(currentSlide - 1);
+    showSlide(currentSlide - 1);
 });
 
 nextButton.addEventListener('click', () => {
-  showSlide(currentSlide + 1);
-  //showSlide(0+1)->1;
-  //showSlide(1+1) ->2;
-  //showSlide(2+1) -> 3;
+    showSlide(currentSlide + 1);
 });
 
 showSlide(currentSlide);
+
+slides[0].addEventListener('mousedown', function(e) {
+  
+    startX = e.clientX;
+});
+
+document.addEventListener('mousemove', function(e) {
+
+        const distance = e.clientX - startX;
+        slides[0].style.transform = `translateX(${distance}px)`;
+
+});
+
+document.addEventListener('mouseup', function() {
+    isDragging = false;
+});
