1ff9b70041192e3c26be725899e4694cf6dbb0da
loafcheck <rachelskim.contact@gmail.com>
2024-02-23 16:58:11 -0800

    lecture44 carousel

--- carousel(lecture44).js
+++ carousel(lecture44).js
@@ -21,6 +21,7 @@ nextButton.addEventListener('click', () => {
 
 showSlide(currentSlide);
 
+
 slides.forEach((slidepage, index)=> {
     slidepage.addEventListener('mousedown', function(e){
         isDragging = true;
@@ -41,14 +42,56 @@ slides.forEach((slidepage, index)=> {
             if (distance >= 100) {
                 showSlide(currentSlide -1);
             } else if (distance <= 100) {
-                console.log(distance);
                 showSlide(currentSlide + 1);
             } else {
                 slidepage.style.transform = 'translateX(0)';
+                return;
             }
+            slidepage.style.transition = 'transform 0.5s ease';
+            setTimeout(()=> {
+                slidepage.style.transition = 'none';
+            }, 500);
         }
     })
 })
+
+
+slides.forEach((slidepage)=> {
+    let touchstartX = 0;
+
+    slidepage.addEventListener('touchstart', function(e){
+        isDragging = true;
+        touchstartX = e.touches[0].clientX;
+    })
+
+    slidepage.addEventListener('touchmove', function(e) {
+        if (isDragging) {
+            const distance = e.touches[0].clientX- touchstartX;
+            slidepage.style.transform = `translateX(${distance}px)`;
+        }
+    })
+
+    slidepage.addEventListener('touchend', function(e){
+        if(isDragging){
+            isDragging = false;
+            const distance = e.changedTouches[0].clientX - touchstartX;
+            if (distance >= 100) {
+                showSlide(currentSlide -1);
+            } else if (distance <= -100) {
+                showSlide(currentSlide + 1);
+            } else {
+                slidepage.style.transform = 'translateX(0)';
+                return;
+            }
+            slidepage.style.transition = 'transform 0.5s ease';
+            setTimeout(()=> {
+                slidepage.style.transition = 'none';
+            }, 500);
+        }
+    })
+})
+
+
 
 /*
 slides[0].addEventListener('mousedown', function(e) {
